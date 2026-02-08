aae62f22824b1828fea48ad0e275e2cf3c8038ee
yassine2734 <elmaghraouiyassine3@gmail.com>
2026-02-08 22:20:05 +0100

    renforcé la config Next/Image pour Blob

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '*.blob.vercel-storage.com',
       },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+      },
     ],
   },
 };
